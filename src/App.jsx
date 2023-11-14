a06eb25e986a45cb7e8e5f6ed50734dacb3f2db0
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-11-15 01:44:56 +0600

    add temp ui and test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import './App.css'
-import conf from "./conf/conf.js";
+import {useState , useEffect } from "react";
+import {useDispatch} from "react-redux";
+import authService from "./appwrite/auth.js";
+import {login,logout} from "./store/authSlice.js";
+import {Footer, Header} from "./components/index.js";
+import {Outlet} from "react-router-dom";
 
 function App() {
-    console.log(conf.appwriteUrl);
-  return (
-    <>
-     <h1>Mega project</h1>
-    </>
-  )
+    const [loading , setLoading ] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        authService.getCurrentAccount(false)
+            .then((userData)=>{
+                if (userData){
+                    dispatch(login(userData))
+                }else {
+                    dispatch(logout()) //It will update our state
+                }
+            })
+            .finally(()=>setLoading(false))
+    }, []);
+
+    return !loading ?(
+        <div className={'min-h-screen flex flex-wrap content-between bg-gray-400'}>
+           <div className={'w-full block'}>
+               <Header/>
+               <main>
+                 TODO<Outlet/>
+               </main>
+               <Footer/>
+           </div>
+        </div>
+    ) : null
+
 }
 
 export default App
